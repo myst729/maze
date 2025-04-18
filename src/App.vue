71315d673405b84dbc5fc73f286fb77c53e8a3d1
Leo Deng <myst.dg@gmail.com>
2025-04-18 19:56:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="128" height="128" />
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+.wrapper {
+  display: flex;
+  place-items: flex-start;
+  flex-wrap: wrap;
+}
+</style>
